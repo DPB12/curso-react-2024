4d4870a071bb5a3aa02d79c32988097d0ddd1c40
daulinhlanz <114731146+daulinhlanz@users.noreply.github.com>
2024-01-09 16:39:09 +0100

    9enero2024

--- Proyectos/Introduccion/src/Inicio.jsx
+++ Proyectos/Introduccion/src/Inicio.jsx
@@ -1,10 +1,16 @@
+import Buscador from "./components/Buscador";
 import Contador from "./components/Contador";
 import RegistrarForm from "./components/RegistrarForm";
 
 const Inicio = () => {
   return (
     <>
+      <Buscador />
+      <br />
+      <br />
       <RegistrarForm />
+      <br />
+      <br />
       <Contador />
     </>
   );
--- Proyectos/Introduccion/src/components/Buscador.jsx
+++ Proyectos/Introduccion/src/components/Buscador.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
 const initialState = ["manzana", "pera", "naranja", "platano", "melon", "uva"];
 
 // Estado inicial que guarde el initialState
 
 const Buscador = () => {
-  const [searchItems, setSearchItems] = useState(second);
+  const [searchItems, setSearchItems] = useState(initialState);
   const [filteredItems, setFilteredItems] = useState(initialState);
+
+  let search = [];
+
+  function handleSearch(event) {
+    event.preventDefault();
+    search = filteredItems.filter((fruta) =>
+      fruta.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+
+    console.log(search);
+    if (search.length > 0) {
+      setSearchItems(search);
+    } else {
+      setSearchItems(filteredItems);
+    }
+  }
+
+  return (
+    /**
+     * Ul con los componentes del array y sobreescribir cuando lo busquemos
+     */
+    <>
+      <input type="text" onKeyUp={handleSearch} />
+      <ul>
+        {searchItems.map((fruta, index) => (
+          <li key={index}>{fruta}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
+
+export default Buscador;
